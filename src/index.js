cb3054ffba0b68d5f465ee30943e73507b4e9f86
shutrup <magagaraev95@mail.ru>
2023-07-02 15:13:15 +0300

    test Commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ const express = require("express");
 require('./db/mongoose')
 const app = express();
 
+const userRouter = require('./routers/user')
+
 const port = process.env.PORT || 3000;
+
+app.use(userRouter)
 
 app.listen(port, () => {
     console.log('Server is up on the port ' + port);
