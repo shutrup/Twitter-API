45e4420f28eeff402480899ff57e8c0669b37685
shutrup <magagaraev95@mail.ru>
2023-07-02 16:16:15 +0300

    user model toJSOn methods

--- src/models/user.js
+++ src/models/user.js
@@ -62,6 +62,15 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+
+    return userObject
+}
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
