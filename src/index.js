8aa745edc1c206e27b1edd74877ed3172186b652
shutrup <magagaraev95@mail.ru>
2023-07-05 18:05:17 +0300

    Create NotificationRouter  and add some requests

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ const app = express();
 
 const userRouter = require('./routers/user')
 const tweetRouter = require('./routers/tweet')
+const notificationRouter = require('./routers/notification')
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(userRouter)
 app.use(tweetRouter)
+app.use(notificationRouter)
 
 app.listen(port, () => {
     console.log('Server is up on the port ' + port);
---
+++ src/routers/notification.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const Notification = require('../models/notification')
+const auth = require('../middleware/auth')
+const router = require('./tweet')
+const e = require('express')
+
+// New Router
+const router = new express.Router()
+
+router.post('/notification', auth, async (req, res) => {
+    const notification = new Notification({ 
+        ...req.body,
+        user: req.user._id
+     })
+
+     try {
+        await notification.save()
+        res.status(201).send(notification)
+     } catch (error) {
+        res.status(400).send(error)
+     }
+
+});
+
+router.get('/notification', async (req, res) => {
+    try {
+        const notification = await Notification.find({})
+        res.send(notification)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+});
+
+router.get('/notification/:id', async (req, res) => {
+
+    const _id = req.params.id;
+
+    try {
+        const notification = await Notification.find({ notReceiverId: _id })
+        res.send(notification)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+});
+
+module.exports = router
+
+
